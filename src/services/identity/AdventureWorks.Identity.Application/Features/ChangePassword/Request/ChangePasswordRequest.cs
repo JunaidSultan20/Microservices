1cddece6906d0d77ae14d5e4ffe4f38457533762
Junaid Sultan <jsu@embrace-it.com>
2025-06-17 17:49:27 +0500

    Add ChangePasswordRequest and example provider classes
    Introduce `ChangePasswordRequest` class to encapsulate data for
    password change operations, implementing `IRequest<ChangePasswordResponse>`.
    Add `ChangePasswordRequestExample` class to provide example instances
    for testing and documentation, implementing `IExamplesProvider<ChangePasswordRequest>`.

---
+++ src/services/identity/AdventureWorks.Identity.Application/Features/ChangePassword/Request/ChangePasswordRequest.cs
@@ -0,0 +1,19 @@
+﻿using AdventureWorks.Identity.Application.Features.ChangePassword.Response;
+
+namespace AdventureWorks.Identity.Application.Features.ChangePassword.Request;
+
+/// <summary>
+/// Represents the request for a change password operation.
+/// </summary>
+/// <remarks>
+/// This class implements the IRequest interface and defines the request for a change password operation.
+/// It includes a ChangePasswordDto property to hold the password change information.
+/// </remarks>
+/// <param name="changePasswordDto">The password information for the change request.</param>
+public class ChangePasswordRequest(ChangePasswordDto changePasswordDto) : IRequest<ChangePasswordResponse>
+{
+    /// <summary>
+    /// Gets the information for the change password request.
+    /// </summary>
+    public ChangePasswordDto ChangePasswordDto { get; } = changePasswordDto;
+}
---
+++ src/services/identity/AdventureWorks.Identity.Application/Features/ChangePassword/Request/ChangePasswordRequestExample.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventureWorks.Identity.Application.Features.ChangePassword.Request;
+
+/// <summary>
+/// Provides examples for the ChangePasswordRequest class.
+/// </summary>
+/// <remarks>
+/// This class implements the IExamplesProvider interface and provides a sample ChangePasswordRequest instance with pre-populated password information.
+/// </remarks>
+public class ChangePasswordRequestExample : IExamplesProvider<ChangePasswordRequest>
+{
+    /// <summary>
+    /// Gets a sample PostLoginRequest instance with username and password.
+    /// </summary>
+    /// <returns>A PostLoginRequest instance with pre-populated authentication information (for example purposes).</returns>
+    public ChangePasswordRequest GetExamples() => new ChangePasswordRequest(new ChangePasswordDto("jane.doe@xyz.com", "janedoe@123", "janeDoe@123"));
+}
