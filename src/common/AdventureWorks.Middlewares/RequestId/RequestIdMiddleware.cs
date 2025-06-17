1dbb1d131da558560286494a7a4eec029a4b128c
Junaid Sultan <jsu@embrace-it.com>
2025-06-17 17:47:46 +0500

    Add RequestIdMiddleware for tracking HTTP request IDs
    Introduced a new namespace `AdventureWorks.Middlewares.RequestId`
    and added the `RequestIdMiddleware` class to implement HTTP
    middleware functionality. The middleware generates a unique
    `requestId` for each request, stores it in `HttpContext.Items`,
    and adds it to the response headers using `Constants.RequestId`.
    The `InvokeAsync` method ensures the middleware integrates
    seamlessly with the pipeline by invoking the next delegate.

---
+++ src/common/AdventureWorks.Middlewares/RequestId/RequestIdMiddleware.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventureWorks.Middlewares.RequestId;
+
+public class RequestIdMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        string requestId = Guid.NewGuid().ToString();
+        context.Items[Constants.RequestId] = requestId;
+        context.Response.Headers[Constants.RequestId] = requestId;
+        await next(context);
+    }
+}
