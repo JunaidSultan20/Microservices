9c9298fc40b708a1c2ecc55af945f91b08b76d29
Junaid Sultan <jsu@embrace-it.com>
2025-06-17 17:49:15 +0500

    Add ChangePasswordHandler for password change requests
    Introduced the `ChangePasswordHandler` class in the `AdventureWorks.Identity.Application.Features.ChangePassword.Handler` namespace to handle password change requests.
    
    The class implements the `IRequestHandler` interface, processing `ChangePasswordRequest` objects and returning `ChangePasswordResponse` objects.
    
    Key logic in the `Handle` method:
    - Retrieves user by email using `userManager.FindByEmailAsync`.
    - Returns `ChangePasswordUnauthorizedResponse` if user is not found.
    - Attempts password change via `userManager.ChangePasswordAsync`.
    - Returns `BadRequestChangePasswordResponse` on failure.
    - Returns `ChangePasswordResponse` with `NoContent` status on success.
    
    References to `ChangePasswordRequest` and `ChangePasswordResponse` classes are included.

---
+++ src/services/identity/AdventureWorks.Identity.Application/Features/ChangePassword/Handler/ChangePasswordHandler.cs
@@ -0,0 +1,25 @@
+﻿using AdventureWorks.Identity.Application.Features.ChangePassword.Request;
+using AdventureWorks.Identity.Application.Features.ChangePassword.Response;
+
+namespace AdventureWorks.Identity.Application.Features.ChangePassword.Handler;
+
+public class ChangePasswordHandler(UserManager<User> userManager) : IRequestHandler<ChangePasswordRequest, ChangePasswordResponse>
+{
+    public async Task<ChangePasswordResponse> Handle(ChangePasswordRequest request,
+                                                     CancellationToken cancellationToken = default)
+    {
+        User? user = await userManager.FindByEmailAsync(request.ChangePasswordDto?.Email ?? string.Empty);
+
+        if (user is null)
+            return new ChangePasswordUnauthorizedResponse();
+
+        IdentityResult result = await userManager.ChangePasswordAsync(user, 
+                                                                      request.ChangePasswordDto?.CurrentPassword ?? string.Empty, 
+                                                                      request.ChangePasswordDto?.NewPassword ?? string.Empty);
+
+        if (!result.Succeeded)
+            return new BadRequestChangePasswordResponse();
+
+        return new ChangePasswordResponse(HttpStatusCode.NoContent, $"{Messages.PasswordUpdatedFor} {request.ChangePasswordDto?.Email}");
+    }
+}
