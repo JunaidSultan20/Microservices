49c69cf08f029f35f6abe080538e20f59ab70460
Junaid Sultan <jsu@embrace-it.com>
2025-06-17 17:44:28 +0500

    Fix terminology in IMessageProducer/Receiver docs
    Updated documentation comments in IMessageProducer and IMessageReceiver interfaces to replace "fanout" with "fan-out" in the `exchangeType` parameter description. This ensures consistent and correct terminology for exchange types.

--- src/common/AdventureWorks.Contracts/Messaging/IMessageProducer.cs
+++ src/common/AdventureWorks.Contracts/Messaging/IMessageProducer.cs
@@ -11,7 +11,7 @@ public interface IMessageProducer
     /// <typeparam name="T">The type of the message to be sent.</typeparam>
     /// <param name="queue">The name of the queue to which the message will be sent.</param>
     /// <param name="exchangeName">The name of the exchange that will route the message.</param>
-    /// <param name="exchangeType">The type of the exchange (e.g., direct, topic, fanout).</param>
+    /// <param name="exchangeType">The type of the exchange (e.g., direct, topic, fan-out).</param>
     /// <param name="routeKey">The routing key used by the exchange to determine which queue(s) should receive the message.</param>
     /// <param name="message">The message to be sent.</param>
     /// <returns>A task representing the asynchronous operation of sending the message.</returns>
--- src/common/AdventureWorks.Contracts/Messaging/IMessageReceiver.cs
+++ src/common/AdventureWorks.Contracts/Messaging/IMessageReceiver.cs
@@ -11,7 +11,7 @@ public interface IMessageReceiver
     /// <typeparam name="T">The type of the message to be received.</typeparam>
     /// <param name="queue">The name of the queue from which the message will be received.</param>
     /// <param name="exchangeName">The name of the exchange that will route the message.</param>
-    /// <param name="exchangeType">The type of the exchange (e.g., direct, topic, fanout).</param>
+    /// <param name="exchangeType">The type of the exchange (e.g., direct, topic, fan-out).</param>
     /// <param name="routeKey">The routing key used by the exchange to determine which queue(s) should receive the message.</param>
     void ReceiveMessage<T>(string queue,
                            string exchangeName,
